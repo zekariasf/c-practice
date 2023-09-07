4448ef08ce6c2ec1541d35028bf5fae6e24fe2d8
zekariasf <zekarias.fikru@gmail.com>
2023-09-07 11:29:57 +0000

    C-practice

---
+++ sizes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int i;
+	double d;
+	char c;
+
+	printf("size of int is %lu.\n", (unsigned long)sizeof(i));
+	printf("size of double is %lu.\n", (unsigned long)sizeof(d));
+	printf("size of char is %lu.\n", (unsigned long)sizeof(c));
+	return (0);
+}
+		
